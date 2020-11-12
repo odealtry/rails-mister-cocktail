1e17673bdc7c0dfbbe6f9350b289406af1554ea9
Oscar Dealtry <odealtry@gmail.com>
2020-11-12 15:01:49 +0000

    models and validations working

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,5 +2,5 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
   belongs_to :cocktail
   validates_uniqueness_of :cocktail_id, scope: :ingredient_id
-  validates :desciption, :cocktail_id, :ingredient_id, presence: true
+  validates :description, :cocktail_id, :ingredient_id, presence: true
 end
