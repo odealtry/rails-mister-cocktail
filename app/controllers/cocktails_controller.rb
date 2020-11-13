aacdc7e408d893d67fe93a48c455f1774f16a60a
Oscar Dealtry <odealtry@gmail.com>
2020-11-13 16:18:22 +0000

    photo behaviour

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -14,7 +14,7 @@ class CocktailsController < ApplicationController
   def create
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
-      redirect_to @cocktail, notice: "Cocktail successfully created"
+      redirect_to cocktails_path, notice: "Cocktail successfully created"
     else
       render :new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'cocktails#index'
-  resources :cocktails, only: [:new, :show, :create] do
+  resources :cocktails, only: [:new, :show, :create, :index] do
     resources :doses, only: [:new, :create]
   end
 
